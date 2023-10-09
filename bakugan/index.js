a65ac2fd5a6a79007d074abfa8135dbabe315a7e
mellywebdev <143849071+mellywebdev@users.noreply.github.com>
2023-10-09 16:54:36 +0100

    Add files via upload

---
+++ bakugan/index.js
@@ -0,0 +1,221 @@
+let slides1 = 30;
+console.log(slides1);
+
+let favFood = "fried rice" ;
+let favDrink = "sprite";
+let favMovie = "xmen";
+
+console.log(favDrink);
+
+favFood = "pasta" ;
+console.log(favFood);
+
+
+const myBirthday = "7th Feb";
+console.log(myBirthday)
+
+
+let js = "cool";
+console.log(js== "cool")
+
+let myList = ["Melvin",true,"Dev Dreamer",1];
+console.log(myList[2]);
+
+
+let industryFinance = 3000
+console.log (industryFinance)
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1+num2);
+console.log(num1%num2);
+
+let total = 10;
+total++;
+console.log(total);
+
+let score = 100;
+score += 100;
+console.log(score);
+
+let balance = 5000;
+balance-= 600;
+console.log(balance);
+
+let donation = 100;
+donation *= 2;
+console.log (donation);
+
+let sweets = 30 ;
+sweets /= 10;
+console.log(sweets);
+
+
+let pizzaSlices = 10;
+pizzaSlices %= 3;
+console.log(pizzaSlices)
+
+
+let x = 30;
+x += 10;
+x/= 2; 
+console.log (x);
+
+let xp = 250;
+xp *= 2;
+console.log(xp);
+
+
+let num = 3;
+let newNum = 3.16;
+
+
+console.log(num);
+console.log(newNum);
+ 
+const PI = 3.1423789;
+
+let num30 = PI.toFixed(3);
+num30 = PI.toPrecision(6)
+console.log(num30);
+
+let lightSpeed = 186000
+
+let num50 = lightSpeed.toExponential(3)
+console.log(num50);
+
+
+
+let email = "melvinlefthanded@gmail.com";
+console.log(email);
+
+let firstName = "Chibuike";
+
+let lastName = "Melvin";
+
+let fullName = firstName+' '+ lastName;
+console.log(fullName[4])  
+
+let day1 = "Good morning, ";
+let night = "Good evening,";
+let question = "How are you?";
+
+let greetingDay = day1 + question;
+let greetingNight = night + question;
+
+console.log(day1+"have you had breakfast?");
+
+
+//let  = prompt("What is your name?")
+
+
+let string = "10";
+let num100 = 5;
+let result = string+ num;
+console.log(typeof result);
+
+
+let pass1 = "i am learning ";
+let pass2 = "Javascript"
+let result1 = pass1 + pass2;
+let result11= pass1.concat(pass2)
+console.log(result1)
+
+
+let title = "Sonic the Hedgehog";
+let platform = "SEGA";
+let year = 1991;
+
+//let info = title + " was released by " + platform + " in the year " + year;
+//console.log(info)
+
+let info2 = `${title} was released by ${platform} in the year ${year}.`;  
+console.log(info2);
+
+let course = "Javascript is cool";
+
+//console.log(course.length);
+
+//console.log(course.trim());
+
+//console.log(course.toUpperCase());
+//console.log(course.toLowerCase())
+//console.log(course.indexOf('a'))
+//console.log(course.lastIndexOf("a"))
+//console.log(course.slice(0,10));
+//console.log(course.replace("not", "so"));
+//console.log(course.charAt(4))
+
+
+
+
+//let currentDate = new Date();
+//console.log(currentDate.getFullYear());
+//let milliseconds = new Date()
+
+
+//let dateString = new Date()
+//let todaysDate = new Date ();
+
+let y = 8 ;
+ 
+console.log(
+    y > 3, y < 6
+);
+
+
+console.log(
+    y== 7 , y == 6
+);
+
+console.log(
+    y=== 7, y=== 6
+);
+
+console.log(
+    y!== 7, y!== 6
+);
+
+let r = 1;
+r = !r;
+
+console.log(r);
+
+let f = 1;
+let g = 0;
+
+
+if(f){
+    console.log("hello")
+};
+
+let z = 5 ;
+
+
+console.log(
+    z > 3, z < 6
+);
+
+console.log(
+    z== 7, z == 6
+);
+
+console.log(
+    z=== 7, z=== 6
+) 
+
+
+
+let s = 10;
+
+let j = 15;
+
+
+console.log(
+    s > j 
+);
+
+console.log(
+    s == "10"
+)
